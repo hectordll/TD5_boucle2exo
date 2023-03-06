52c837b589241cb06336646b768457eb24dac55f
HectorD <contactdealh@gmail.com>
2023-03-06 11:21:36 +0100

    fix region

--- register.php
+++ register.php
@@ -3,6 +3,11 @@
 $csvData = array_map('str_getcsv', file('assets/departments_regions_france_2016.csv'));
 // suppression de la ligne d'entête du tableau
 $deps_regions = array_slice($csvData, 1);
+$region = [];
+foreach($deps_regions as $dep_region){
+    if ($dep_region[2] != 'NULL' && !in_array($dep_region[2], $region));
+    array_push($region, $dep_region[2]);
+};
 ?>
 
 <!DOCTYPE html>
@@ -34,14 +39,35 @@ $deps_regions = array_slice($csvData, 1);
         <div class="row justify-content-center">
             <div class="col-md-5">
 
-            <form action="">
-                <div>
-                    <label for="name">Nom :</label>
-                    <input type="text" id="name" name="user_name">
-
-                    <input type="date" id="start" name="trip-start"
+            <form action="" method="">
+                <div class="form-group">
+                    <label for="name">Nom :</label></br>
+                    <input class="form-control" type="text" id="name" name="user_name">
+                </div>
+                <div class="form-group">
+                    <label for="birthdate">Date de naissance</label></br>
+                    <input class="form-control" type="date" id="start" name="trip-start"
                     value="2018-07-22"
                     min="2018-01-01" max="2018-12-31">
+                </div>
+                <div class="form-group">
+                    <label for="departement">Département</label></br>
+                    <select class="form-control" name="departement" id="departement">
+                        <option value="">Selectionner votre département</option>
+                    <?php foreach($deps_regions as $dep_region){
+                        echo("<option value='$dep_region[0]'>$dep_region[1]</option>");
+                    } ?>
+                    </select>
+                </div>
+                    </br>
+                <div class="form-group">
+                    <label for="region">Région</label></br>
+                    <select class="form-control" name="region" id="region">
+                        <option value="">Selectionner votre région</option>
+                    <?php foreach($deps_regions as $region){
+                            echo("<option value='$region[2]'>$region[3]</option>");
+                    } ?>
+                    </select>
                 </div>
             </form>
 
